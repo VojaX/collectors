55c883bd70022326203ce0e413bc31ca9d593bdb
Jacek Góraj <jckgrj@gmail.com>
2017-01-25 21:07:12 +0100

    changed collection list overview and improved design

--- app/collection-list/collection-list.component.ts
+++ app/collection-list/collection-list.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
 	templateUrl: 'app/collection-list/collection-list.component.html'
 })
 export class CollectionListComponent {
-	//TODO: import products from endpoint /api/collections
 	collections: any[] = [
 		{
 			"category": "money",
@@ -14,7 +13,7 @@ export class CollectionListComponent {
 			"name": "Polskie banknoty z lat 1925 - 1929",
 			"set": "Ser. X. , Ser. X.X. oraz Ser. XX",
 			"count": 3,
-			"numbersRange": "",
+			"numeration": "",
 			"releaseDate": "20 august 1926 and 20 august 1929",
 			"country": "Poland",
 			"description": "Water mark: image of Bolesław Chrobry and dates: 992 and 1125 or 10 ZŁ. Size: 160 mm x 80 mm",
@@ -29,9 +28,9 @@ export class CollectionListComponent {
 			"type": "tazos",
 			"subtype": "pokemon",
 			"name": "Pokemon Tazo 1",
-			"set": "1",
+			"set": "Set 1",
 			"count": 51,
-			"numbersRange": "1 - 51",
+			"numeration": "1 - 51",
 			"releaseDate": "2000 - 2001",
 			"country": "Poland",
 			"description": "Made of plastic in a shape of a circle. Has a diameter of 40mm and thickness of 1mm. They are painted by paint/ink. Easy to clash and smooth. On the pogs you can see pokemons from range of 1 - 151 (pokedex numeration) These pokemons are only the one which can evolve (in their base form) and Ash (Tazo 51). These tazos have got a bug in translation on their front. Instead of using polish 'Typ' which means 'Type' they used polish 'Siła' which means 'Force'.",
