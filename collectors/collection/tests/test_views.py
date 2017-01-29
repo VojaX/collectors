1b4dea14f9e6fe5c7299fc261af7d981a8a661f1
Iwo Czerniawski <iwoczerniawski@gmail.com>
2017-01-29 16:51:22 +0100

    Add fixtures to tests

--- collectors/collection/tests/test_views.py
+++ collectors/collection/tests/test_views.py
@@ -1,9 +1,17 @@
+from cities_light.models import Country, City
 from django.core.urlresolvers import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
+from collectors.collection.factories import CollectionFactory
+
 
 class CollectionListAPIViewTests(APITestCase):
+    fixtures = [
+        'fixtures/test_countries',
+        'fixtures/test_regions',
+        'fixtures/test_cities',
+    ]
 
     def test_list(self):
         CollectionFactory.create_batch(3)
@@ -11,15 +19,22 @@ class CollectionListAPIViewTests(APITestCase):
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data['results']), 3)
+        self.assertEqual(len(response.data), 3)
 
 
 class CollectionDetailAPIViewTests(APITestCase):
+    fixtures = [
+        'fixtures/test_countries',
+        'fixtures/test_regions',
+        'fixtures/test_cities',
+    ]
+
     def test_detail(self):
-        collection = CollectionFactory.create_batch()
+        collection = CollectionFactory()
         url = reverse('api.collection:detail', kwargs={
             'pk': collection.pk
         })
+
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data['results']), 3)
+        self.assertEqual(response.data['id'], collection.id)
