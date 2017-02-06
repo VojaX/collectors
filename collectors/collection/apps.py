941ea063ce69a26e236c04d4c43a3ae63a0854e8
Iwo Czerniawski <iwoczerniawski@gmail.com>
2017-02-06 14:25:26 +0100

    Add default values for FK in collections. Add CollectionSerializer

--- collectors/collection/apps.py
+++ collectors/collection/apps.py
@@ -1,1 +1,1 @@
-from django.apps import AppConfigfrom django.utils.translation import ugettext_lazy as _class CollectionConfig(AppConfig):    name = 'collectors.collection'    verbose_name = _('Kolekcje')
+from django.apps import AppConfigfrom django.utils.translation import ugettext_lazy as _class CollectionConfig(AppConfig):    name = 'collectors.collection'    verbose_name = _('Collections')
