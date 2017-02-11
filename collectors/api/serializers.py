537327dc511db6adc0a81039b2d04444222006b9
Iwo <iwoczerniawski@gmail.com>
2017-02-11 17:58:02 +0100

    Update serializers.py
    Fix format

--- collectors/api/serializers.py
+++ collectors/api/serializers.py
@@ -1,1 +1,22 @@
-from cities_light.loading import get_cities_modelsfrom rest_framework import serializersfrom rest_framework import fieldsCountry, Region, City = get_cities_models()class CountrySerializer(serializers.ModelSerializer):    """    Serializer for Country.    """    code = fields.CharField(source='code2')    class Meta:        model = Country        fields = (            'id',            'name',            'name_ascii',            'code',        )
+from cities_light.loading import get_cities_models
+from rest_framework import serializers
+from rest_framework import fields
+
+Country, Region, City = get_cities_models()
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    """
+    Serializer for Country.
+    """
+    code = fields.CharField(source='code2')
+
+    class Meta:
+        model = Country
+        fields = (
+            'id',
+            'name',
+            'name_ascii',
+            'code',
+        )
+        
