dc8ecc2e131188bd452f6a9a84713921ce6e7a19
Iwo <iwoczerniawski@gmail.com>
2017-02-11 17:56:34 +0100

    Update __init__.py
    Fix format

--- collectors/settings/__init__.py
+++ collectors/settings/__init__.py
@@ -1,1 +1,8 @@
-from .base import *try:    from .local import *except ImportError:    import warnings    warnings.warn('Use "settings/local.py" to override settings')
+from .base import *
+try:
+    from .local import *
+except ImportError:
+    import warnings
+    warnings.warn('Use "settings/local.py" to override settings')
+
+    
