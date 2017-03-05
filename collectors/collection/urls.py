3e6f60dda144e62c8a1e8f3e310e07a25ac6509b
Iwo <iwoczerniawski@gmail.com>
2017-03-05 16:35:22 +0100

    Update urls.py
    fix coding

--- collectors/collection/urls.py
+++ collectors/collection/urls.py
@@ -1,1 +1,16 @@
-from django.conf.urls import urlfrom . import viewsapi_urls = [    url(        r'^$',        views.CollectionListAPIView.as_view(),        name='list'    ),    url(        r'^(?P<pk>[0-9]+)/$',        views.CollectionDetailAPIView.as_view(),        name='detail'    ),]
+from django.conf.urls import url
+from . import views
+
+
+api_urls = [
+    url(
+        r'^$',
+        views.CollectionListAPIView.as_view(),
+        name='list'
+    ),
+    url(
+        r'^(?P<pk>[0-9]+)/$',
+        views.CollectionDetailAPIView.as_view(),
+        name='detail'
+    ), 
+]
