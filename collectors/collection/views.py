d80b2d3634404b79a0ff8a85db2eca43477a45f9
Iwo <iwoczerniawski@gmail.com>
2017-03-05 16:34:50 +0100

    Update views.py
    fix coding

--- collectors/collection/views.py
+++ collectors/collection/views.py
@@ -1,1 +1,20 @@
-from rest_framework import genericsfrom collectors.collection.models import Collectionfrom collectors.collection.serializers import CollectionListSerializerclass CollectionListAPIView(generics.ListAPIView):    """    Collection list endpoint    """    queryset = Collection.objects.all()    serializer_class = CollectionListSerializerclass CollectionDetailAPIView(generics.RetrieveAPIView):    """    Collection detail endpoint    """    queryset = Collection.objects.all()    serializer_class = CollectionListSerializer
+from rest_framework import generics
+
+from collectors.collection.models import Collection
+from collectors.collection.serializers import CollectionListSerializer
+
+
+class CollectionListAPIView(generics.ListAPIView):
+    """
+    Collection list endpoint
+    """
+    queryset = Collection.objects.all()
+    serializer_class = CollectionListSerializer
+
+
+class CollectionDetailAPIView(generics.RetrieveAPIView):
+    """
+    Collection detail endpoint
+    """
+    queryset = Collection.objects.all()
+    serializer_class = CollectionListSerializer 
